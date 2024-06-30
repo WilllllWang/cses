fd854c1431f7756c35a081cfa15da35af8ec7936
WilllllWang <151107396+WilllllWang@users.noreply.github.com>
2024-06-30 16:40:45 +0800

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -2,30 +2,10 @@
     
 using namespace std;
 using ll = long long;
-const ll mod = 1e9 + 7;
 
 
 void solve() {
-    // n is number of coins
-    // x is target num
-    int n, x;
-    cin >> n >> x;
-    vector<ll> coins(n);
-    for (auto& c: coins) {
-        cin >> c;
-    }
-
-    vector<ll> dp(x + 1, 0);
-    dp[0] = 1;
-    for (auto& c: coins) {              // Check for every coin's possible path 
-        for (int i = 1; i <= x; i++) {  
-            if (i - c >= 0) {
-                dp[i] += dp[i - c];
-                dp[i] %= mod;
-            }
-        } 
-    }
-    cout << dp[x] << endl;
+    
 }
 
 
